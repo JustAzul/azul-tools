21610993028fe90eb057053a46953fb38fa17040
Azul <diego.kfu@hotmail.com>
2021-01-16 18:38:30 -0300

    Code Optimization

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const Log = require('./components/Logger.js');
-const {storeFile, storeChatData} = require('./components/FileStore.js');
-const {readJSON, sleep, TimeStamp, isURL, isTradeOfferURL, GetSteamID64FromURL, isSteamID64, isValidSteamID, formatNumber, SplitArray} = require('azul-helper');
+const AzulFileStore = require('./components/FileStore.js');
+const AzulHelper = require('azul-helper');
 const Tester = require('./components/Tester.js');
 const Cache = require('./components/Cache.js');
 
 module.exports = {
     Log: Log,
-    storeFile: storeFile,
-    storeChatData: storeChatData,
-    readJSON: readJSON,
-    sleep: sleep,
-    TimeStamp: TimeStamp,
-    isURL: isURL,
-    isTradeOfferURL: isTradeOfferURL,
-    GetSteamID64FromURL: GetSteamID64FromURL,
-    isSteamID64: isSteamID64,
-    isValidSteamID: isValidSteamID,
-    formatNumber: formatNumber,
-    SplitArray: SplitArray,
+    ...AzulFileStore,
+    ...AzulHelper,
     Cache: Cache,
     Tester: Tester
 }
