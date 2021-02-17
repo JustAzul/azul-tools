f15b5687e98d5a179d74c6e67a05411ef117cff9
Azul <diego.kfu@hotmail.com>
2021-02-17 01:21:58 -0300

    readJSONSync

--- index.d.ts
+++ index.d.ts
@@ -16,6 +16,7 @@ declare namespace AzulHelper {
     export function GetSteamID64FromURL(str: string): Promise<string | null>;
     export function formatNumber(number?: number): string;
     export function readJSON(Filepath: PathLike): Promise<Object>;
+    export function readJSONSync(Filepath: PathLike): Promise<Object>;
     export function sleep(ms: number): Promise<void>;
     export function SplitArray(Array: [], MaxSize: number): Promise<any[][]>;
     export function storeFile(filePath: PathLike, content: string | NodeJS.ArrayBufferView, flag?: string): Promise<void>;    
