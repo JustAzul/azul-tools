8a8e813acea85ce2fe73e9c83be757a6345b57fe
Azul <diego.kfu@hotmail.com>
2021-05-01 00:41:01 -0300

    Pattern()

--- dist/index.js
+++ dist/index.js
@@ -4,13 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const azul_helper_1 = __importDefault(require("azul-helper"));
+const fs_1 = __importDefault(require("fs"));
 const Logger_1 = __importDefault(require("./components/Logger"));
 const storeChatData_1 = __importDefault(require("./components/storeChatData"));
 const Cache_1 = __importDefault(require("./components/Cache"));
+function Pattern() {
+    const encoding = 'utf-8';
+    const Graphic = fs_1.default.readFileSync('./Pattern.txt', { encoding });
+    console.log(Graphic);
+}
 exports.default = {
     Log: Logger_1.default,
     storeChatData: storeChatData_1.default,
     Cache: Cache_1.default,
     ...azul_helper_1.default,
+    Pattern,
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSw4REFBaUM7QUFDakMsaUVBQXNDO0FBRXRDLCtFQUF1RDtBQUV2RCwrREFBdUM7QUFFdkMsa0JBQWU7SUFDYixHQUFHLEVBQUgsZ0JBQUc7SUFDSCxhQUFhLEVBQWIsdUJBQWE7SUFDYixLQUFLLEVBQUwsZUFBSztJQUNMLEdBQUcscUJBQU07Q0FDVixDQUFDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSw4REFBaUM7QUFDakMsNENBQW9CO0FBQ3BCLGlFQUFzQztBQUV0QywrRUFBdUQ7QUFFdkQsK0RBQXVDO0FBRXZDLFNBQVMsT0FBTztJQUNkLE1BQU0sUUFBUSxHQUFHLE9BQU8sQ0FBQztJQUN6QixNQUFNLE9BQU8sR0FBRyxZQUFFLENBQUMsWUFBWSxDQUFDLGVBQWUsRUFBRSxFQUFFLFFBQVEsRUFBRSxDQUFDLENBQUM7SUFDL0QsT0FBTyxDQUFDLEdBQUcsQ0FBQyxPQUFPLENBQUMsQ0FBQztBQUN2QixDQUFDO0FBRUQsa0JBQWU7SUFDYixHQUFHLEVBQUgsZ0JBQUc7SUFDSCxhQUFhLEVBQWIsdUJBQWE7SUFDYixLQUFLLEVBQUwsZUFBSztJQUNMLEdBQUcscUJBQU07SUFDVCxPQUFPO0NBQ1IsQ0FBQyJ9
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import Helper from 'azul-helper';
+import fs from 'fs';
 import Log from './components/Logger';
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import storeChatData from './components/storeChatData';
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import Cache from './components/Cache';
 
+function Pattern() {
+  const encoding = 'utf-8';
+  const Graphic = fs.readFileSync('./Pattern.txt', { encoding });
+  // eslint-disable-next-line no-console
+  console.log(Graphic);
+}
+
 export default {
   Log,
   storeChatData,
   Cache,
   ...Helper,
+  Pattern,
 };
