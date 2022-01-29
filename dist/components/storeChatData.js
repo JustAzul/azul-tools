166a6d1a721d66728f8fbe6b9f6299497c72237b
Azul <diego.kfu@hotmail.com>
2022-01-29 02:55:14 -0300

    v1.9.24 build

--- dist/components/storeChatData.js
+++ dist/components/storeChatData.js
@@ -1,12 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const os_1 = require("os");
-const Helper_1 = require("./Helper");
+const Helper_1 = __importDefault(require("./Helper"));
 async function storeChatData(UserID64, Message, Bot = false, server_timestamp, BaseDir = 'history') {
-    const ts = await (0, Helper_1.TimeStamp)(new Date(server_timestamp));
+    const ts = await Helper_1.default.TimeStamp(new Date(server_timestamp));
     const filePath = `${BaseDir}/chat/${ts.Date}/${UserID64}.log`;
     const line = `[${ts.Time}] ${Bot ? 'B' : 'U'}: ${Message}${os_1.EOL}`;
-    return (0, Helper_1.storeFile)(filePath, line);
+    return Helper_1.default.storeFile(filePath, line);
 }
 exports.default = storeChatData;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RvcmVDaGF0RGF0YS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9jb21wb25lbnRzL3N0b3JlQ2hhdERhdGEudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFFQSwyQkFBeUI7QUFFekIscUNBQWdEO0FBR2hELEtBQUssVUFBVSxhQUFhLENBQUMsUUFBZ0IsRUFBRSxPQUFlLEVBQUUsR0FBRyxHQUFHLEtBQUssRUFBRSxnQkFBd0IsRUFBRSxPQUFPLEdBQUcsU0FBUztJQUN4SCxNQUFNLEVBQUUsR0FBRyxNQUFNLElBQUEsa0JBQVMsRUFBQyxJQUFJLElBQUksQ0FBQyxnQkFBZ0IsQ0FBQyxDQUFDLENBQUM7SUFDdkQsTUFBTSxRQUFRLEdBQUcsR0FBRyxPQUFPLFNBQVMsRUFBRSxDQUFDLElBQUksSUFBSSxRQUFRLE1BQU0sQ0FBQztJQUM5RCxNQUFNLElBQUksR0FBRyxJQUFJLEVBQUUsQ0FBQyxJQUFJLEtBQUssR0FBRyxDQUFDLENBQUMsQ0FBQyxHQUFHLENBQUMsQ0FBQyxDQUFDLEdBQUcsS0FBSyxPQUFPLEdBQUcsUUFBRyxFQUFFLENBQUM7SUFDakUsT0FBTyxJQUFBLGtCQUFTLEVBQUMsUUFBUSxFQUFFLElBQUksQ0FBQyxDQUFDO0FBQ25DLENBQUM7QUFFRCxrQkFBZSxhQUFhLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RvcmVDaGF0RGF0YS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9jb21wb25lbnRzL3N0b3JlQ2hhdERhdGEudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFFQSwyQkFBeUI7QUFDekIsc0RBQThCO0FBRzlCLEtBQUssVUFBVSxhQUFhLENBQUMsUUFBZ0IsRUFBRSxPQUFlLEVBQUUsR0FBRyxHQUFHLEtBQUssRUFBRSxnQkFBd0IsRUFBRSxPQUFPLEdBQUcsU0FBUztJQUN4SCxNQUFNLEVBQUUsR0FBRyxNQUFNLGdCQUFNLENBQUMsU0FBUyxDQUFDLElBQUksSUFBSSxDQUFDLGdCQUFnQixDQUFDLENBQUMsQ0FBQztJQUM5RCxNQUFNLFFBQVEsR0FBRyxHQUFHLE9BQU8sU0FBUyxFQUFFLENBQUMsSUFBSSxJQUFJLFFBQVEsTUFBTSxDQUFDO0lBQzlELE1BQU0sSUFBSSxHQUFHLElBQUksRUFBRSxDQUFDLElBQUksS0FBSyxHQUFHLENBQUMsQ0FBQyxDQUFDLEdBQUcsQ0FBQyxDQUFDLENBQUMsR0FBRyxLQUFLLE9BQU8sR0FBRyxRQUFHLEVBQUUsQ0FBQztJQUNqRSxPQUFPLGdCQUFNLENBQUMsU0FBUyxDQUFDLFFBQVEsRUFBRSxJQUFJLENBQUMsQ0FBQztBQUMxQyxDQUFDO0FBRUQsa0JBQWUsYUFBYSxDQUFDIn0=
