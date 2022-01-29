80a707410ff293a98668871c96c936efedde90c2
Azul <diego.kfu@hotmail.com>
2022-01-29 01:16:35 -0300

    added types

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import figlet from 'figlet';
-// eslint-disable-next-line import/no-unresolved, import/extensions
 import Helper from './components/Helper';
 import Log from './components/Logger';
-// eslint-disable-next-line import/no-unresolved, import/extensions
 import storeChatData from './components/storeChatData';
-// eslint-disable-next-line import/no-unresolved, import/extensions
 import Cache from './components/Cache';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const colour = require('colour');
 
 function Pattern(): void {
-  const o = {
+  const o: figlet.Options = {
     horizontalLayout: 'full',
     verticalLayout: 'full',
   };
 
-  // @ts-expect-error im lazy and im not adding this type
   const text = figlet.textSync('Justazul.com', o);
   // eslint-disable-next-line no-console
   console.log(colour.cyan(text));
