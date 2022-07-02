d88bb98274db8bf12fe8ff1e6f6101a0a06918bc
Azul <diego.kfu@hotmail.com>
2022-07-02 14:42:32 -0300

    First Commit

---
+++ tests/cache.test.ts
@@ -0,0 +1,36 @@
+import tools from '..';
+
+describe('test Cache', () => {
+    test('Set/Get', () => {
+      const key = Math.random().toString();
+      const value = Math.random();
+  
+      tools.Cache.Set(key, value, Number.MAX_SAFE_INTEGER)
+  
+      expect(
+        tools.Cache.Get(key)
+      ).toBe(value)
+    })
+  
+    
+    test('Set/Get Expiration', () => {
+      const key = Math.random().toString();
+      const value = Math.random();
+  
+      const expireTime = 1000;
+  
+      jest.useFakeTimers();
+      jest.spyOn(global, 'setTimeout');
+  
+      tools.Cache.Set(key, value, expireTime)
+  
+      expect(setTimeout).toHaveBeenCalledTimes(1);
+      expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), expireTime);
+
+      jest.runAllTimers();
+  
+      expect(
+        tools.Cache.Get(key)
+      ).toBe(undefined)
+    })
+  })
